a020a9737e2bb8851c30d87643419278f1aa0f3e
2005101 <88779300+2005101@users.noreply.github.com>
2024-10-28 13:51:27 -0400

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,9 @@
+x=input("what is the answer to the great question of life, the universe and everthing?")
+if x=="42":
+    print("Yes")
+elif x=="forty-two":
+    print("Yes")
+elif x=="forty two":
+    print("Yes")
+else:
+    print("No")
